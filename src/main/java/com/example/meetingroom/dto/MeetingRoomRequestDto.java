cc34794d0ddc5526c2169a36eb1786976b5750c4
byeongdoohan <quden04@gmail.com>
2025-08-07 22:51:03 +0900

    feat: meetingroom dto 추가

---
+++ src/main/java/com/example/meetingroom/dto/MeetingRoomRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.meetingroom.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class MeetingRoomRequestDto {
+    private final String name;
+    private final int capacity;
+    private final BigDecimal pricePerHour;
+
+    @Builder
+    public MeetingRoomRequestDto(final String name, final int capacity, final BigDecimal pricePerHour) {
+        this.name = name;
+        this.capacity = capacity;
+        this.pricePerHour = pricePerHour;
+    }
+}
