093e7bb28fedefebc1fe0dba018395cb1be5fc29
byeongdoohan <quden04@gmail.com>
2025-08-08 04:07:23 +0900

    chore: @NoArgsConstructor 추가, name 에 unique 추가

--- src/main/java/com/example/meetingroom/entity/MeetingRoom.java
+++ src/main/java/com/example/meetingroom/entity/MeetingRoom.java
@@ -1,19 +1,22 @@
 package com.example.meetingroom.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MeetingRoom {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
     @Column(nullable = false)
     private int capacity;
