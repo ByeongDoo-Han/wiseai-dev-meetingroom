c1c65a126ab45867abbb6fc6826a622ece1b2048
byeongdoohan <quden04@gmail.com>
2025-08-08 22:01:24 +0900

    feat: member repository 추가

---
+++ src/main/java/com/example/meetingroom/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.meetingroom.repository;
+
+import com.example.meetingroom.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
