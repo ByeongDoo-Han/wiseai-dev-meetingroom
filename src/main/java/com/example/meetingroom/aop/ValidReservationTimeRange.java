228478d697775c284a78809d8ed9de575ee933fe
byeongdoohan <quden04@gmail.com>
2025-08-09 17:13:50 +0900

    feat: reservation 시간 validation 추가

---
+++ src/main/java/com/example/meetingroom/aop/ValidReservationTimeRange.java
@@ -0,0 +1,19 @@
+package com.example.meetingroom.aop;
+
+import com.example.meetingroom.util.TimeRangeValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TimeRangeValidator.class)
+public @interface ValidReservationTimeRange {
+    String message() default "종료 시간은 시작 시간보다 뒤여야 합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
