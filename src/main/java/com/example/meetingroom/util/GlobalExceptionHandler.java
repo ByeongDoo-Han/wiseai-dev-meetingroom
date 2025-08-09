2f6a8596c3f92bbde1bf1e7f3c011d6def1c033b
byeongdoohan <quden04@gmail.com>
2025-08-09 22:45:01 +0900

    feat: reservation crud 추가

--- src/main/java/com/example/meetingroom/util/GlobalExceptionHandler.java
+++ src/main/java/com/example/meetingroom/util/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.example.meetingroom.util;
 
 import com.example.meetingroom.exception.CustomException;
 import com.example.meetingroom.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,7 +22,24 @@ public class GlobalExceptionHandler {
             .data(null)
             .build();
 
-        return ResponseEntity.ok()
+        return ResponseEntity
+            .status(errorCode.getHttpStatus())
+            .body(body);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomResponseEntity<Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        CustomResponseEntity<Object> body = CustomResponseEntity.builder()
+            .success(false)
+            .message(errorMessage)
+            .httpStatus(HttpStatus.BAD_REQUEST)
+            .data(null)
+            .build();
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
             .body(body);
     }
 }
