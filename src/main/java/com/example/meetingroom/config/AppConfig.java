3b681f82f51015d82c556e80cc19d80843d14acc
byeongdoohan <quden04@gmail.com>
2025-08-19 22:46:45 +0900

    refactor: payment gateway map 추가

--- src/main/java/com/example/meetingroom/config/AppConfig.java
+++ src/main/java/com/example/meetingroom/config/AppConfig.java
@@ -1,8 +1,15 @@
 package com.example.meetingroom.config;
 
+import com.example.meetingroom.entity.PaymentProviderType;
+import com.example.meetingroom.service.PaymentGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +17,12 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Map<PaymentProviderType, PaymentGateway> paymentGatewayMap(List<PaymentGateway> gateways){
+        return gateways.stream()
+            .collect(Collectors.toMap(PaymentGateway::getProviderType,
+                Function.identity()));
     }
 }
