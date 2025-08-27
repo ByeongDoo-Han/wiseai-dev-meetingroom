91d3286156c5412e1bcf4ae8fe070f18f27f936b
byeongdoohan <quden04@gmail.com>
2025-08-27 23:54:41 +0900

    refactor: 정적 팩토리 메서드 적용

--- src/main/java/com/example/meetingroom/entity/Reservation.java
+++ src/main/java/com/example/meetingroom/entity/Reservation.java
@@ -1,13 +1,14 @@
 package com.example.meetingroom.entity;
 
+import com.example.meetingroom.dto.reservation.ReservationRequestDto;
 import com.example.meetingroom.exception.CustomException;
 import com.example.meetingroom.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -15,7 +16,7 @@ import java.time.temporal.ChronoUnit;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class Reservation {
+public class Reservation extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,68 +43,61 @@ public class Reservation {
     @Column(nullable = false)
     private PaymentStatus paymentStatus;
 
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
     public void updatePaymentStatus(PaymentStatus newStatus) {
         this.paymentStatus = newStatus;
     }
 
-    public void update(LocalDateTime newStartTime, LocalDateTime newEndTime, BigDecimal newTotalAmount, MeetingRoom newMeetingRoom) {
+    public void update(LocalDateTime newStartTime, LocalDateTime newEndTime, MeetingRoom newMeetingRoom) {
         this.startTime = newStartTime;
         this.endTime = newEndTime;
-        this.totalAmount = newTotalAmount;
+        valid(newStartTime, newEndTime);
         this.meetingRoom = newMeetingRoom;
+        this.totalAmount = calculateTotalPrice(newMeetingRoom.getPricePerHour());
     }
 
-    public void valid(){
-        validStartTimeMinuteZeroOrThirty();
-        validEndTimeMinuteZeroOrThirty();
-        validStartTimeIsBeforeThanEndTime();
-        validIsNotZero();
+    private void valid(LocalDateTime startTime, LocalDateTime endTime) {
+        validStartAndEndTimeMinuteZeroOrThirty(startTime, endTime);
+        validStartTimeIsBeforeThanEndTime(startTime, endTime);
+        validIsNotZero(startTime, endTime);
     }
 
-    public void validStartTimeIsBeforeThanEndTime(){
-        if(this.startTime.isAfter(this.endTime)){
+    private void validStartTimeIsBeforeThanEndTime(LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime.isAfter(endTime)) {
             throw new CustomException(ErrorCode.RESERVATION_START_TIME_IS_AFTER_THAN_END_TIME);
         }
     }
 
-    public void validStartTimeMinuteZeroOrThirty(){
-        if(this.startTime.getMinute() != 0 && this.startTime.getMinute()!=30){
+    private void validStartAndEndTimeMinuteZeroOrThirty(LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime.getMinute() != 0 && startTime.getMinute() != 30 &&
+            endTime.getMinute() != 0 && endTime.getMinute() != 30) {
             throw new CustomException(ErrorCode.INVALID_RESERVATION_TIME);
         }
     }
 
-    public void validEndTimeMinuteZeroOrThirty(){
-        if(this.endTime.getMinute() != 0 && this.endTime.getMinute()!=30){
-            throw new CustomException(ErrorCode.INVALID_RESERVATION_TIME);
-        }
-    }
-
-    public void validIsNotZero(){
-        if(endTime.isEqual(startTime)){
+    private void validIsNotZero(LocalDateTime startTime, LocalDateTime endTime) {
+        if (endTime.isEqual(startTime)) {
             throw new CustomException(ErrorCode.MEETING_ROOM_TIME_IS_ZERO);
         }
     }
 
-    public BigDecimal calculateTotalPrice(BigDecimal pricePerHour) {
+    private BigDecimal calculateTotalPrice(BigDecimal pricePerHour) {
         long hours = ChronoUnit.HOURS.between(this.startTime, this.endTime);
         long minutes = ChronoUnit.MINUTES.between(this.startTime, this.endTime) % 60;
-        BigDecimal totalHours = BigDecimal.valueOf(hours).add(BigDecimal.valueOf(minutes).divide(BigDecimal.valueOf(60), 2, BigDecimal.ROUND_HALF_UP));
+        BigDecimal totalHours = BigDecimal.valueOf(hours).add(BigDecimal.valueOf(minutes).divide(BigDecimal.valueOf(60), 2, RoundingMode.HALF_UP));
         return pricePerHour.multiply(totalHours);
     }
 
-    @Builder
-    public Reservation(final Long id, final Member member, final MeetingRoom meetingRoom, final LocalDateTime startTime, final LocalDateTime endTime, final BigDecimal totalAmount, final PaymentStatus paymentStatus) {
-        this.id = id;
+    private Reservation(final Member member, final MeetingRoom meetingRoom, final LocalDateTime startTime, final LocalDateTime endTime) {
         this.member = member;
         this.meetingRoom = meetingRoom;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.paymentStatus = paymentStatus;
-        valid();
+        this.paymentStatus = PaymentStatus.PENDING;
+        valid(startTime, endTime);
         this.totalAmount = calculateTotalPrice(meetingRoom.getPricePerHour());
+    }
+
+    public static Reservation create(ReservationRequestDto dto, Member member, MeetingRoom meetingRoom) {
+        return new Reservation(member, meetingRoom, dto.getStartTime(), dto.getEndTime());
     }
 }
